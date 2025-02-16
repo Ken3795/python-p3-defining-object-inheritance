617f3ed829786303a99f8ccf908a62d0097b8eae
Kenneth Kipkoech <kenkipkoech57@gmail.com>
2025-02-16 15:45:40 +0300

    lab completed

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,10 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+    def __init__(self, wheel_size, wheel_number):
+        # Car inherits from Vehicle and calls the parent constructor.
+        super().__init__(wheel_size, wheel_number)
+
+    def go(self):
+        # Overriding the go method to make a different "vroom" sound.
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,13 @@
 class Vehicle:
-    pass
+    def __init__(self, wheel_size, wheel_number):
+        # The __init__ method initializes the object with attributes.
+        self.wheel_size = wheel_size
+        self.wheel_number = wheel_number
+
+    def go(self):
+        # The go method should return a "vroom" sound.
+        return "vrrrrrrrooom!"
+
+    def fill_up_tank(self):
+        # The fill_up_tank method returns a message.
+        return "filling up!"
